fc6828e827e73f1f20794fb671828af281118677
JayvonK <jaykhuth@gmail.com>
2024-01-29 10:38:14 -0800

    lecture done

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,8 @@ let digimonStatus = document.getElementById("digimonStatus");
 let digimonInput = document.getElementById("digimonInput");
 let favoriteBtn = document.getElementById("favoriteBtn");
 
+let digimon = "";
+
 
 const digimonApi = async (digimon) => {
     const promise = await fetch('https://digimon-api.vercel.app/api/digimon/name/' + digimon);
@@ -19,7 +21,7 @@ digimonInput.addEventListener('keydown', async (event) => {
     //On enter I want this function to run
     if(event.key === 'Enter')
     {
-        let digimon = await digimonApi(event.target.value);
+        digimon = await digimonApi(event.target.value);
         digimonImg.src = digimon[0].img;
         digimonName.textContent = digimon[0].name;
         digimonStatus.textContent = digimon[0].level;
@@ -27,7 +29,7 @@ digimonInput.addEventListener('keydown', async (event) => {
 })
 
 favoriteBtn.addEventListener('click', (event) => {
-    
+    saveToLocalStorage(digimon[0].name);
 })
 
 //https://digimon-api.vercel.app/api/digimon
