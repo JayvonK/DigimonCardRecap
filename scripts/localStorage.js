6d2a0ae1324056658189ef1c1f4b37ec0569c9bb
JayvonK <jaykhuth@gmail.com>
2024-01-29 09:43:26 -0800

    removelocalstorage

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,23 +1,38 @@
 const saveToLocalStorage = (digimon) => {
+    //favorites will get the current values in local storage
+    //AKA saves the array in favorites
+    let favorites = getLocalStorage();
 
-    let favorites = getlocalStorage();
-
-    localStorage.setItem("Favorites", json.stringify(digimon));
+    //If the name is already included in the local storage we will not push into favorites
+    if(!favorites.includes(digimon)){
+        favorites.push(digimon);
+    }
+    //JSON.stringify This ensures what ever we savei nto local storage is a string
+    localStorage.setItem("Favorites", JSON.stringify(favorites));
 }
 
 const getLocalStorage = () => {
-
+    //Getting our values from local storage
     let localStorageData = localStorage.getItem("Favorites");
 
+    //We check if that data is null if so we return an empty array
     if(localStorageData === null)
     {
         return [];
     }
 
-    return json.parse(localStorageData);
+    //We return an array of local storage
+    return JSON.parse(localStorageData);
 
 }
 
-const removeFromLocalStorage = () => {
+const removeFromLocalStorage = (digimon) => {
+    let favorites = getLocalStorage();
+
+    let namedIndex = favorites.indexOf(digimon);
+
+    favorites.splice(namedIndex, 1);
+
+    localStorage.setItem("Favorites", JSON.stringify(favorites));
 
 }
