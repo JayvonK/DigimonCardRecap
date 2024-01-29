e44f3b34ce7b9d9aa94dacaee930594d54739c20
JayvonK <jaykhuth@gmail.com>
2024-01-29 09:24:19 -0800

    setting local storages

---
+++ scripts/localStorage.js
@@ -0,0 +1,14 @@
+const saveToLocalStorage = (digimon) => {
+
+    let favorites = getlocalStorage();
+
+    localStorage.setItem("Favorites", json.stringify(digimon));
+}
+
+const getLocalStorage = () => {
+
+}
+
+const removeFromLocalStorage = () => {
+
+}
