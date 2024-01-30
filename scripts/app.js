e499bb484122e3c02f806efbbc14e5dd073caff8
JayvonK <jaykhuth@gmail.com>
2024-01-30 10:10:13 -0800

    lecture finish

--- scripts/app.js
+++ scripts/app.js
@@ -30,8 +30,7 @@ digimonApi2();
 
 digimonInput.addEventListener('keydown', async (event) => {
     //On enter I want this function to run
-    if(event.key === 'Enter')
-    {
+    if (event.key === 'Enter') {
         digimon = await digimonApi(event.target.value);
         digimonImg.src = digimon[0].img;
         digimonName.textContent = digimon[0].name;
@@ -50,10 +49,48 @@ getFavoritesBtn.addEventListener('click', () => {
     //Clears getFavoritesDiv so the Array display will not constantly repeat.
     getFavoritesDiv.textContent = "";
 
-    favorites.map(digimonName => {
+    favorites.map(digiName => {
+        //creating a p tag dynamically
         let p = document.createElement("p");
-        p.textContent = digimonName;
+
+        //Setting its textContent to digiName
+        p.textContent = digiName;
+
+        //className replaces all classes with new classes
+        p.className = "text-lg font-medium text-gray-900 dark:text-white";
+
+        //Creating a button dynamically
+        let button = document.createElement("button");
+        button.type = "button";
+        button.textContent = "X";
+        
+        //classList alows us to be more concise it doesn't replace all classes.
+        button.classList.add(
+            "text-gray-400",
+            "bg-transparent",
+            "hover:bg-gray-200",
+            "hover:text-gray-900",
+            "rounded-lg",
+            "text-sm",
+            "w-8",
+            "h-8",
+            "justify-end",
+            "dark:hover:bg-gray-600",
+            "dark:hover:text-white"
+        );
+
+        //creating and addEventListener for our button which removes digiName from our favorites
+        button.addEventListener('click', () => {
+
+            removeFromLocalStorage(digiName);
+
+            p.remove();
+        })
+        //appending our button to our p-tag
+        p.append(button);
+        //appending our p-tag to our favoritesDiv
         getFavoritesDiv.append(p);
+
     })
 })
 
